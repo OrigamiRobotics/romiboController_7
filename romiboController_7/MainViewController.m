a59dd0e5fb127cf9da8acda36f0e28001c9c2725
Danny Brown <daniel.brown@tobi.com>
2014-10-05 08:39:58 -0700

    align toolbar icons for palette and buttons

--- romiboController_7/MainViewController.m
+++ romiboController_7/MainViewController.m
@@ -773,7 +773,7 @@ const CGFloat kButtonInset_y =   4.0;
   if ([[User sharedUserInstance] isLoggedIn]){
     [self.toolbarContainerView setHidden:NO];
     [self.loginButton setHidden:YES];
-    NSString *loggedInInfo = [NSString stringWithFormat:@"Logged in as %@", [[User sharedUserInstance] name]];
+    NSString *loggedInInfo = [NSString stringWithFormat:@"%@", [[User sharedUserInstance] name]];
     self.logedInInfoLabel.text = loggedInInfo;
   } else {
     [self.toolbarContainerView setHidden:YES];
